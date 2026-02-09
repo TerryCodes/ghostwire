795db9631f71cfec82d2ede231768f8ade4aa86e
FrenchToblerone54 <FrenchToblerone54@users.noreply.github.com>
2026-02-09 23:23:21 +0330

    Bump version to v0.4.4

--- updater.py
+++ updater.py
@@ -23,7 +23,7 @@ class Updater:
     def get_current_version(self):
         script_path=Path(sys.argv[0])
         if script_path.name.startswith(f"ghostwire-{self.component_name}"):
-            return "v0.4.3"
+            return "v0.4.4"
         return "dev"
 
     async def check_for_update(self):
